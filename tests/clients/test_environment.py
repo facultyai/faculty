f189215a5b34e9216c895e0f5d3ab76510b5f1d2
Setrak Balian <setrak.b@asidatascience.com>
2018-11-29 23:17:49 +0000

    Add schema and list tests.

---
+++ tests/clients/test_environment.py
@@ -0,0 +1,78 @@
+# Copyright 2018 ASI Data Science
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
+import pytest
+import uuid
+
+from dateutil.tz import UTC
+from marshmallow import ValidationError
+
+
+from sherlockml.clients.environment import (
+    Environment,
+    EnvironmentClient,
+    EnvironmentSchema,
+)
+
+from tests.clients.fixtures import PROFILE
+
+
+PROJECT_ID = uuid.uuid4()
+
+
+ENVIRONMENT = Environment(
+    id=uuid.uuid4(),
+    project_id=PROJECT_ID,
+    name="Test Environment Name",
+    author_id=uuid.uuid4(),
+    description="Looking forward to testing environments",
+    created_at=datetime.datetime(2018, 10, 3, 4, 20, 0, 0, tzinfo=UTC),
+    updated_at=datetime.datetime(2018, 11, 3, 4, 21, 15, 0, tzinfo=UTC),
+)
+
+
+ENVIRONMENT_BODY = {
+    "environmentId": str(ENVIRONMENT.id),
+    "name": ENVIRONMENT.name,
+    "description": ENVIRONMENT.description,
+    "authorId": str(ENVIRONMENT.author_id),
+    "projectId": str(PROJECT_ID),
+    "createdAt": "2018-10-03T04:20:00Z",
+    "updatedAt": "2018-11-03T04:21:15Z",
+}
+
+
+def test_environment_schema():
+    data = EnvironmentSchema().load(ENVIRONMENT_BODY)
+    assert data == ENVIRONMENT
+    with pytest.raises(ValidationError):
+        EnvironmentSchema().load({})
+
+
+def test_environment_client_list(mocker):
+    mocker.patch.object(EnvironmentClient, "_get", return_value=[ENVIRONMENT])
+    schema_mock = mocker.patch(
+        "sherlockml.clients.environment.EnvironmentSchema"
+    )
+
+    client = EnvironmentClient(PROFILE)
+
+    assert client.list(PROJECT_ID) == [ENVIRONMENT]
+
+    schema_mock.assert_called_once_with(many=True)
+
+    EnvironmentClient._get.assert_called_once_with(
+        "/project/{}/environment".format(PROJECT_ID), schema_mock.return_value
+    )
