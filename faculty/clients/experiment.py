eaa23a013112e7bfb63ad239a0b76c2a71f6cd34
Andrew Crozier <wacrozier@gmail.com>
2019-02-19 16:11:54 +0000

    Add experiment model, schema and tests

---
+++ faculty/clients/experiment.py
@@ -0,0 +1,50 @@
+# Copyright 2018-2019 Faculty Science Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from collections import namedtuple
+
+from marshmallow import fields, post_load
+
+from faculty.clients.base import BaseSchema
+
+
+Experiment = namedtuple(
+    "Experiment",
+    [
+        "id",
+        "name",
+        "description",
+        "artifact_location",
+        "created_at",
+        "last_updated_at",
+        "deleted_at",
+    ],
+)
+
+
+class ExperimentSchema(BaseSchema):
+    id = fields.Integer(data_key="experimentId", required=True)
+    name = fields.String(required=True)
+    description = fields.String(required=True)
+    artifact_location = fields.String(
+        data_key="artifactLocation", required=True
+    )
+    created_at = fields.DateTime(data_key="createdAt", required=True)
+    last_updated_at = fields.DateTime(data_key="lastUpdatedAt", required=True)
+    deleted_at = fields.DateTime(data_key="deletedAt", missing=None)
+
+    @post_load
+    def make_experiment(self, data):
+        return Experiment(**data)
---
+++ tests/clients/test_experiment.py
@@ -0,0 +1,60 @@
+# Copyright 2018-2019 Faculty Science Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from datetime import datetime
+
+import pytest
+from pytz import UTC
+from marshmallow import ValidationError
+
+from faculty.clients.experiment import Experiment, ExperimentSchema
+
+
+CREATED_AT = datetime(2018, 3, 10, 11, 32, 6, 247000, tzinfo=UTC)
+CREATED_AT_STRING = "2018-03-10T11:32:06.247Z"
+LAST_UPDATED_AT = datetime(2018, 3, 10, 11, 32, 30, 172000, tzinfo=UTC)
+LAST_UPDATED_AT_STRING = "2018-03-10T11:32:30.172Z"
+DELETED_AT = datetime(2018, 3, 10, 11, 37, 42, 482000, tzinfo=UTC)
+DELETED_AT_STRING = "2018-03-10T11:37:42.482Z"
+
+
+EXPERIMENT = Experiment(
+    id=12,
+    name="experiment name",
+    description="experiment description",
+    artifact_location="https://example.com",
+    created_at=CREATED_AT,
+    last_updated_at=LAST_UPDATED_AT,
+    deleted_at=DELETED_AT,
+)
+EXPERIMENT_BODY = {
+    "experimentId": EXPERIMENT.id,
+    "name": EXPERIMENT.name,
+    "description": EXPERIMENT.description,
+    "artifactLocation": EXPERIMENT.artifact_location,
+    "createdAt": CREATED_AT_STRING,
+    "lastUpdatedAt": LAST_UPDATED_AT_STRING,
+    "deletedAt": DELETED_AT_STRING,
+}
+
+
+def test_experiment_schema():
+    data = ExperimentSchema().load(EXPERIMENT_BODY)
+    assert data == EXPERIMENT
+
+
+def test_experiment_schema_invalid():
+    with pytest.raises(ValidationError):
+        ExperimentSchema().load({})
