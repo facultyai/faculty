7ad6c327b5f5afc2d21cf9be8af23472921d3496
Maitiu O Ciarain <m.ociarain@gmail.com>
2019-03-11 17:23:08 +0000

    Add patch method to base client

--- faculty/clients/base.py
+++ faculty/clients/base.py
@@ -171,6 +171,13 @@ class BaseClient(object):
         response = self._put_raw(endpoint, **kwargs)
         return _deserialise_response(schema, response)
 
+    def _patch_raw(self, endpoint, *args, **kwargs):
+        return self._request("PATCH", endpoint, *args, **kwargs)
+
+    def _patch(self, endpoint, schema, **kwargs):
+        response = self._patch_raw(endpoint, **kwargs)
+        return _deserialise_response(schema, response)
+
     def _delete_raw(self, endpoint, *args, **kwargs):
         return self._request("DELETE", endpoint, *args, **kwargs)
 
