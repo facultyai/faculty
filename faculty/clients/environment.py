4471259bb5b410f8418025a8ad6d02521e53d0e5
Ares Meroueh <ares.m@asidatascience.com>
2019-04-19 12:37:51 +0200

    Enhance environment client
    The environment client currently only returns high level
    metadata about an environment. It also only allows one to
    list environments from a project; more advanced operations
    are not possible. This commit enhances this client
    in several ways:
    
    * The "specification" field is added to EnvironmentSchema
    
    * A number of schemas (and associated objects) are introduced
    to serialise/deserialise the "specification" field
    
    * New methods are added to EnvironmentClient (get, create,
    update, delete)
    
    * Additional schemas (and associated objects) are
    introduced to support the new methods of EnvironmentClient
    (EnvironmentUpdateSchema, EnvironmentCreationResponseSchema)

--- faculty/clients/environment.py
+++ faculty/clients/environment.py
@@ -14,11 +14,38 @@
 
 
 from collections import namedtuple
+from enum import Enum
 
-from marshmallow import fields, post_load
+from marshmallow import fields, post_dump, post_load
+from marshmallow_enum import EnumField
 
-from faculty.clients.base import BaseSchema, BaseClient
+from faculty.clients.base import BaseClient, BaseSchema
 
+
+class Constraint(Enum):
+    AT_LEAST = ">="
+    EQUAL = "=="
+
+
+Version = namedtuple("Version", ["constraint", "identifier"])
+
+PythonPackage = namedtuple("PythonPackage", ["name", "version"])
+
+Pip = namedtuple("Pip", ["extra_index_urls", "packages"])
+
+Conda = namedtuple("Conda", ["channels", "packages"])
+
+PythonSpecification = namedtuple("PythonSpecification", ["pip", "conda"])
+
+Apt = namedtuple("Apt", ["packages"])
+
+AptPackage = namedtuple("AptPackage", ["name"])
+
+Script = namedtuple("Script", ["script"])
+
+Python = namedtuple("Python", ["python_2", "python_3"])
+
+Specification = namedtuple("Specification", ["apt", "bash", "python"])
 
 Environment = namedtuple(
     "Environment",
@@ -30,8 +57,125 @@ Environment = namedtuple(
         "author_id",
         "created_at",
         "updated_at",
+        "specification",
     ],
 )
+
+EnvironmentUpdate = namedtuple(
+    "EnvironmentUpdate", ["name", "description", "specification"]
+)
+
+
+class SemverSchema(BaseSchema):
+    constraint = EnumField(Constraint, by_value=True, required=True)
+    identifier = fields.String(required=True)
+
+    @post_load
+    def make_version(self, data):
+        return Version(**data)
+
+
+class VersionField(fields.Field):
+    """
+    Field that serialises/deserialises a Python package version.
+    """
+
+    def _deserialize(self, value, attr, obj, **kwargs):
+        if value == "latest":
+            return "latest"
+        else:
+            return SemverSchema().load(value)
+
+    def _serialize(self, value, attr, obj, **kwargs):
+        if value == "latest":
+            return "latest"
+        else:
+            return SemverSchema().dump(value)
+
+
+class PythonPackageSchema(BaseSchema):
+    name = fields.String(required=True)
+    version = VersionField(required=True)
+
+    @post_load
+    def make_python_package(self, data):
+        return PythonPackage(**data)
+
+
+class PipSchema(BaseSchema):
+    extra_index_urls = fields.List(
+        fields.String(), data_key="extraIndexUrls", required=True, many=True
+    )
+    packages = fields.List(fields.Nested(PythonPackageSchema()), required=True)
+
+    @post_load
+    def make_pip(self, data):
+        return Pip(**data)
+
+
+class CondaSchema(BaseSchema):
+    channels = fields.List(fields.String(), required=True)
+    packages = fields.List(fields.Nested(PythonPackageSchema()), required=True)
+
+    @post_load
+    def make_conda(self, data):
+        return Conda(**data)
+
+
+class PythonSpecificationSchema(BaseSchema):
+    conda = fields.Nested(CondaSchema(), required=True)
+    pip = fields.Nested(PipSchema(), required=True)
+
+    @post_load
+    def make_python_specification(self, data):
+        return PythonSpecification(**data)
+
+
+class PythonSchema(BaseSchema):
+    python_2 = fields.Nested(
+        PythonSpecificationSchema(), required=True, data_key="Python2"
+    )
+    python_3 = fields.Nested(
+        PythonSpecificationSchema(), required=True, data_key="Python3"
+    )
+
+    @post_load
+    def make_python(self, data):
+        return Python(**data)
+
+
+class AptPackageSchema(BaseSchema):
+    name = fields.String(required=True)
+
+    @post_load
+    def make_apt_package(self, data):
+        return AptPackage(**data)
+
+
+class AptSchema(BaseSchema):
+    packages = fields.List(fields.Nested(AptPackageSchema()), required=True)
+
+    @post_load
+    def make_apt(self, data):
+        return Apt(**data)
+
+
+class ScriptSchema(BaseSchema):
+    script = fields.String(required=True)
+
+    @post_load
+    def make_script(self, data):
+        return Script(**data)
+
+
+class SpecificationSchema(BaseSchema):
+    apt = fields.Nested(AptSchema(), required=True)
+    bash = fields.List(fields.Nested(ScriptSchema()), required=True)
+    python = fields.Nested(PythonSchema(), required=True)
+
+    @post_load
+    def make_specification(self, data):
+        return Specification(**data)
 
 
 class EnvironmentSchema(BaseSchema):
@@ -42,10 +186,39 @@ class EnvironmentSchema(BaseSchema):
     author_id = fields.UUID(data_key="authorId", required=True)
     created_at = fields.DateTime(data_key="createdAt", required=True)
     updated_at = fields.DateTime(data_key="updatedAt", required=True)
+    specification = fields.Nested(SpecificationSchema(), required=True)
 
     @post_load
     def make_environment(self, data):
         return Environment(**data)
+
+
+class EnvironmentUpdateSchema(BaseSchema):
+    name = fields.String(required=True)
+    description = fields.String(required=True)
+    specification = fields.Nested(SpecificationSchema(), required=True)
+
+    @post_load
+    def make_environment_update(self, data):
+        return EnvironmentUpdate(**data)
+
+
+class EnvironmentCreationResponseSchema(BaseSchema):
+    id = fields.UUID(data_key="environmentId", required=True)
+    project_id = fields.UUID(data_key="projectId", required=True)
+    name = fields.String(required=True)
+    description = fields.String(required=True)
+    author_id = fields.UUID(data_key="authorId", required=True)
+    specification = fields.Nested(SpecificationSchema(), required=True)
+
+    @post_load
+    def make_environment(self, data):
+        return Environment(**data, created_at=None, updated_at=None)
+
+
+class DummySchema(BaseSchema):
+    def load(self, in_data):
+        return None
 
 
 class EnvironmentClient(BaseClient):
@@ -55,3 +228,33 @@ class EnvironmentClient(BaseClient):
     def list(self, project_id):
         endpoint = "/project/{}/environment".format(project_id)
         return self._get(endpoint, EnvironmentSchema(many=True))
+
+    def get(self, project_id, environment_id):
+        endpoint = "/project/{}/environment/{}".format(
+            project_id, environment_id
+        )
+        return self._get(endpoint, EnvironmentSchema())
+
+    def update(self, project_id, environment_id, update):
+        endpoint = "/project/{}/environment/{}".format(
+            project_id, environment_id
+        )
+        return self._put(
+            endpoint,
+            DummySchema(),
+            json=EnvironmentUpdateSchema().dump(update),
+        )
+
+    def create(self, project_id, content):
+        endpoint = "/project/{}/environment".format(project_id)
+        return self._post(
+            endpoint,
+            EnvironmentCreationResponseSchema(),
+            json=EnvironmentUpdateSchema().dump(content),
+        )
+
+    def delete(self, project_id, environment_id):
+        endpoint = "/project/{}/environment/{}".format(
+            project_id, environment_id
+        )
+        return self._delete(endpoint, schema=DummySchema())
--- tests/clients/test_environment.py
+++ tests/clients/test_environment.py
@@ -20,22 +20,115 @@ from dateutil.tz import UTC
 from marshmallow import ValidationError
 
 from faculty.clients.environment import (
+    Apt,
+    AptPackage,
+    AptPackageSchema,
+    AptSchema,
+    Conda,
+    CondaSchema,
+    Constraint,
     Environment,
     EnvironmentClient,
+    EnvironmentCreationResponseSchema,
     EnvironmentSchema,
+    EnvironmentUpdate,
+    EnvironmentUpdateSchema,
+    Pip,
+    PipSchema,
+    Python,
+    PythonPackage,
+    PythonPackageSchema,
+    PythonSchema,
+    PythonSpecification,
+    PythonSpecificationSchema,
+    Script,
+    ScriptSchema,
+    SemverSchema,
+    Specification,
+    SpecificationSchema,
+    Version,
 )
 
+VERSION = Version(constraint=Constraint.EQUAL, identifier="1.0.0")
+VERSION_LATEST = "latest"
+
+VERSION_BODY = {"constraint": "==", "identifier": "1.0.0"}
+VERSION_BODY_LATEST = "latest"
+
+PYTHON_PACKAGE = PythonPackage(name="tensorflow", version=VERSION)
+PYTHON_PACKAGE_BODY = {"name": "tensorflow", "version": VERSION_BODY}
+
+PYTHON_PACKAGE_LATEST = PythonPackage(
+    name="tensorflow", version=VERSION_LATEST
+)
+PYTHON_PACKAGE_BODY_LATEST = {
+    "name": "tensorflow",
+    "version": VERSION_BODY_LATEST,
+}
+
+PIP = Pip(extra_index_urls=["Some URL"], packages=[PYTHON_PACKAGE])
+
+PIP_BODY = {"extraIndexUrls": ["Some URL"], "packages": [PYTHON_PACKAGE_BODY]}
+
+CONDA_BODY = {"channels": ["Some channel"], "packages": [PYTHON_PACKAGE_BODY]}
+
+CONDA = Conda(channels=["Some channel"], packages=[PYTHON_PACKAGE])
+
+PYTHON_SPECIFICATION_BODY = {"pip": PIP_BODY, "conda": CONDA_BODY}
+
+PYTHON_SPECIFICATION = PythonSpecification(conda=CONDA, pip=PIP)
+
+APT_PACKAGE_BODY = {"name": "cuda"}
+
+APT_PACKAGE = AptPackage(name="cuda")
+
+APT_BODY = {"packages": [APT_PACKAGE_BODY]}
+
+APT = Apt(packages=[APT_PACKAGE])
+
+PYTHON_BODY = {
+    "Python2": PYTHON_SPECIFICATION_BODY,
+    "Python3": PYTHON_SPECIFICATION_BODY,
+}
+
+PYTHON = Python(python_2=PYTHON_SPECIFICATION, python_3=PYTHON_SPECIFICATION)
+
+SCRIPT_STR = "# Edit your script\n"
+
+SCRIPT_BODY = {"script": SCRIPT_STR}
+
+SCRIPT = Script(script=SCRIPT_STR)
+
+SPECIFICATION_BODY = {
+    "apt": APT_BODY,
+    "bash": [SCRIPT_BODY],
+    "python": PYTHON_BODY,
+}
+
+SPECIFICATION = Specification(apt=APT, bash=[SCRIPT], python=PYTHON)
+
+ENVIRONMENT_UPDATE_BODY = {
+    "name": "Test",
+    "description": "A test environment",
+    "specification": SPECIFICATION_BODY,
+}
+
+ENVIRONMENT_UPDATE = EnvironmentUpdate(
+    name="Test", description="A test environment", specification=SPECIFICATION
+)
 
 PROJECT_ID = uuid.uuid4()
+ENVIRONMENT_ID = uuid.uuid4()
 
 ENVIRONMENT = Environment(
-    id=uuid.uuid4(),
+    id=ENVIRONMENT_ID,
     project_id=PROJECT_ID,
     name="Test Environment",
     description="Environment description",
     author_id=uuid.uuid4(),
     created_at=datetime.datetime(2018, 10, 3, 4, 20, 0, 0, tzinfo=UTC),
     updated_at=datetime.datetime(2018, 11, 3, 4, 21, 15, 0, tzinfo=UTC),
+    specification=SPECIFICATION,
 )
 
 ENVIRONMENT_BODY = {
@@ -46,7 +139,93 @@ ENVIRONMENT_BODY = {
     "authorId": str(ENVIRONMENT.author_id),
     "createdAt": "2018-10-03T04:20:00Z",
     "updatedAt": "2018-11-03T04:21:15Z",
+    "specification": SPECIFICATION_BODY,
 }
+
+ENVIRONMENT_CREATION_RESPONSE = Environment(
+    id=uuid.uuid4(),
+    project_id=PROJECT_ID,
+    name="Test Environment",
+    description="Environment description",
+    author_id=uuid.uuid4(),
+    created_at=None,
+    updated_at=None,
+    specification=SPECIFICATION,
+)
+
+ENVIRONMENT_CREATION_RESPONSE_BODY = {
+    "environmentId": str(ENVIRONMENT_CREATION_RESPONSE.id),
+    "projectId": str(PROJECT_ID),
+    "name": ENVIRONMENT_CREATION_RESPONSE.name,
+    "description": ENVIRONMENT_CREATION_RESPONSE.description,
+    "authorId": str(ENVIRONMENT_CREATION_RESPONSE.author_id),
+    "specification": SPECIFICATION_BODY,
+}
+
+
+def test_semver_schema():
+    data = SemverSchema().load(VERSION_BODY)
+    assert data == VERSION
+
+
+def test_python_package_schema():
+    data = PythonPackageSchema().load(PYTHON_PACKAGE_BODY)
+    assert data == PYTHON_PACKAGE
+
+    data = PythonPackageSchema().load(PYTHON_PACKAGE_BODY_LATEST)
+    assert data == PYTHON_PACKAGE_LATEST
+
+
+def test_pip_schema():
+    data = PipSchema().load(PIP_BODY)
+    assert data == PIP
+
+
+def test_conda_schema():
+    data = CondaSchema().load(CONDA_BODY)
+    assert data == CONDA
+
+
+def test_python_specification_schema():
+    data = PythonSpecificationSchema().load(PYTHON_SPECIFICATION_BODY)
+    assert data == PYTHON_SPECIFICATION
+
+
+def test_apt_package_schema():
+    data = AptPackageSchema().load(APT_PACKAGE_BODY)
+    assert data == APT_PACKAGE
+
+
+def test_apt_schema():
+    data = AptSchema().load(APT_BODY)
+    assert data == APT
+
+
+def test_python_schema():
+    data = PythonSchema().load(PYTHON_BODY)
+    assert data == PYTHON
+
+
+def test_script_schema():
+    data = ScriptSchema().load(SCRIPT_BODY)
+    assert data == SCRIPT
+
+
+def test_specification_schema():
+    data = SpecificationSchema().load(SPECIFICATION_BODY)
+    assert data == SPECIFICATION
+
+
+def test_environment_update_schema():
+    data = EnvironmentUpdateSchema().load(ENVIRONMENT_UPDATE_BODY)
+    assert data == ENVIRONMENT_UPDATE
+
+
+def test_environment_creation_response_schema():
+    data = EnvironmentCreationResponseSchema().load(
+        ENVIRONMENT_CREATION_RESPONSE_BODY
+    )
+    assert data == ENVIRONMENT_CREATION_RESPONSE
 
 
 def test_environment_schema():
@@ -69,4 +248,76 @@ def test_environment_client_list(mocker):
     schema_mock.assert_called_once_with(many=True)
     EnvironmentClient._get.assert_called_once_with(
         "/project/{}/environment".format(PROJECT_ID), schema_mock.return_value
+    )
+
+
+def test_environment_client_get(mocker):
+    mocker.patch.object(EnvironmentClient, "_get", return_value=ENVIRONMENT)
+    schema_mock = mocker.patch("faculty.clients.environment.EnvironmentSchema")
+
+    client = EnvironmentClient(mocker.Mock())
+    assert client.get(PROJECT_ID, ENVIRONMENT_ID) == ENVIRONMENT
+
+    schema_mock.assert_called_once_with()
+    EnvironmentClient._get.assert_called_once_with(
+        "/project/{}/environment/{}".format(PROJECT_ID, ENVIRONMENT_ID),
+        schema_mock.return_value,
+    )
+
+
+def test_environment_client_update(mocker):
+    mocker.patch.object(EnvironmentClient, "_put", return_value=None)
+    mocker.patch.object(
+        EnvironmentUpdateSchema, "dump", return_value=ENVIRONMENT_UPDATE_BODY
+    )
+    schema_mock = mocker.patch("faculty.clients.environment.DummySchema")
+
+    client = EnvironmentClient(mocker.Mock())
+    assert (
+        client.update(PROJECT_ID, ENVIRONMENT_ID, ENVIRONMENT_UPDATE) is None
+    )
+
+    EnvironmentUpdateSchema.dump.assert_called_once_with(ENVIRONMENT_UPDATE)
+    EnvironmentClient._put.assert_called_once_with(
+        "/project/{}/environment/{}".format(PROJECT_ID, ENVIRONMENT_ID),
+        schema_mock.return_value,
+        json=ENVIRONMENT_UPDATE_BODY,
+    )
+
+
+def test_environment_client_create(mocker):
+    mocker.patch.object(
+        EnvironmentClient, "_post", return_value=ENVIRONMENT_CREATION_RESPONSE
+    )
+    mocker.patch.object(
+        EnvironmentUpdateSchema, "dump", return_value=ENVIRONMENT_UPDATE_BODY
+    )
+    schema_mock = mocker.patch(
+        "faculty.clients.environment.EnvironmentCreationResponseSchema"
+    )
+
+    client = EnvironmentClient(mocker.Mock())
+    assert (
+        client.create(PROJECT_ID, ENVIRONMENT_UPDATE)
+        == ENVIRONMENT_CREATION_RESPONSE
+    )
+
+    EnvironmentUpdateSchema.dump.assert_called_once_with(ENVIRONMENT_UPDATE)
+    EnvironmentClient._post.assert_called_once_with(
+        "/project/{}/environment".format(PROJECT_ID),
+        schema_mock.return_value,
+        json=ENVIRONMENT_UPDATE_BODY,
+    )
+
+
+def test_environment_client_delete(mocker):
+    mocker.patch.object(EnvironmentClient, "_delete", return_value=None)
+    schema_mock = mocker.patch("faculty.clients.environment.DummySchema")
+
+    client = EnvironmentClient(mocker.Mock())
+    assert client.delete(PROJECT_ID, ENVIRONMENT_ID) is None
+
+    EnvironmentClient._delete.assert_called_once_with(
+        "/project/{}/environment/{}".format(PROJECT_ID, ENVIRONMENT_ID),
+        schema=schema_mock.return_value,
     )
