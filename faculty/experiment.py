c36e1cf418be3fe71b8f04a181bf1de02c098ea9
Andrew Crozier <wacrozier@gmail.com>
2019-06-19 11:21:41 +0100

    Refactoring, add filter helper and add tests

--- faculty/experiment.py
+++ faculty/experiment.py
@@ -20,33 +20,21 @@ from faculty.session import get_session
 from faculty._util.resolvers import resolve_project_id
 from faculty.clients.experiment import ExperimentClient
 
-
-class QueryResult(object):
-    def __init__(self, iterable):
-        self.iterable = iterable
-
-    def __iter__(self):
-        return iter(self.iterable)
-
-
-class ExperimentRunQueryResult(QueryResult):
-    def as_dataframe(self):
-        records = []
-        for run in self:
-            row = {
-                "Experiment ID": run.experiment_id,
-                "Run ID": run.id,
-                "Status": run.status.value,
-                "Started At": run.started_at,
-            }
-            for metric in run.metrics:
-                row[metric.key] = row[metric.value]
-            records.append(row)
-        return pandas.DataFrame(records)
+from faculty.clients.experiment import (
+    ComparisonOperator,
+    DeletedAtFilter,
+    ExperimentIdFilter,
+    MetricFilter,
+    ParamFilter,
+    RunIdFilter,
+    TagFilter,
+)
 
 
 @attrs
 class ExperimentRun(object):
+    """A single run of an experiment."""
+
     id = attrib()
     run_number = attrib()
     experiment_id = attrib()
@@ -67,6 +55,52 @@ class ExperimentRun(object):
 
     @classmethod
     def query(cls, project=None, filter=None, sort=None, **session_config):
+        """Query the platform for experiment runs.
+
+        Parameters
+        ----------
+        project : str, UUID, or None
+            The name or ID of a project. If ``None`` is passed (the default),
+            the project will be inferred from the runtime context.
+        filter : a filter object from ``faculty.clients.experiment``
+            Condition(s) to filter experiment runs by. ``FilterBy`` provides a
+            convenience interface for constructing filter objects.
+        sort : a sequence of sort objects from ``faculty.clients.experiment``
+            Condition(s) to sort experiment runs by.
+        **session_config
+            Configuration options to build the session with.
+
+        Returns
+        -------
+        ExperimentRunList
+
+        Examples
+        --------
+        Get all experiment runs in the current project:
+
+        >>> ExperimentRun.query()
+        ExperimentRunList([ExperimentRun(...)])
+
+        Get all experiment runs in a named project:
+
+        >>> ExperimentRun.query("my project")
+        ExperimentRunList([ExperimentRun(...)])
+
+        Filter experiment runs by experiment ID:
+
+        >>> ExperimentRun.query(filter=FilterBy.experiment_id() == 2)
+        ExperimentRunList([ExperimentRun(...)])
+
+        Filter experiment runs by a more complex condition:
+
+        >>> filter = (
+        ...     FilterBy.experiment_id().one_of([2, 3, 4]) &
+        ...     (FilterBy.metric("accuracy") > 0.9) &
+        ...     (FilterBy.param("alpha") < 0.3)
+        ... )
+        >>> ExperimentRun.query("my project", filter)
+        ExperimentRunList([ExperimentRun(...)])
+        """
 
         session = get_session(**session_config)
         project_id = resolve_project_id(session, project)
@@ -75,7 +109,6 @@ class ExperimentRun(object):
             client = ExperimentClient(session)
 
             response = client.query_runs(project_id, filter, sort)
-            # return map(cls._from_client_model, response.runs)
             yield from map(cls._from_client_model, response.runs)
 
             while response.pagination.next is not None:
@@ -88,4 +121,193 @@ class ExperimentRun(object):
                 )
                 yield from map(cls._from_client_model, response.runs)
 
-        return ExperimentRunQueryResult(list(_get_runs()))
+        return ExperimentRunList(_get_runs())
+
+
+class ExperimentRunList(list):
+    """A list of experiment runs.
+
+    This collection is a subclass of ``list``, and so supports all its
+    functionality, but adds the ``as_dataframe`` method which returns a
+    representation of the contained ExperimentRuns as a ``pandas.DataFrame``.
+    """
+
+    def __repr__(self):
+        return "{}({})".format(
+            self.__class__.__name__, super(ExperimentRunList, self).__repr__()
+        )
+
+    def as_dataframe(self):
+        """Get the experiment runs as a pandas DataFrame.
+
+        Returns
+        -------
+        pandas.DataFrame
+        """
+
+        records = []
+        for run in self:
+            row = {
+                ("experiment_id", ""): run.experiment_id,
+                ("run_id", ""): run.id,
+                ("run_number", ""): run.run_number,
+                ("status", ""): run.status.value,
+                ("started_at", ""): run.started_at,
+                ("ended_at", ""): run.ended_at,
+            }
+            for param in run.params:
+                row[("params", param.key)] = param.value
+            for metric in run.metrics:
+                row[("metrics", metric.key)] = metric.value
+            records.append(row)
+
+        df = pandas.DataFrame(records)
+        df.columns = pandas.MultiIndex.from_tuples(df.columns)
+
+        # Reorder columns and return
+        return df[
+            [
+                "experiment_id",
+                "run_id",
+                "run_number",
+                "status",
+                "started_at",
+                "ended_at",
+                "params",
+                "metrics",
+            ]
+        ]
+
+
+class _FilterBuilder(object):
+    def __init__(self, constructor, *constructor_args):
+        self.constructor = constructor
+        self.constructor_args = constructor_args
+
+    def _build(self, *args):
+        return self.constructor(*(self.constructor_args + args))
+
+    def defined(self, value=True):
+        return self._build(ComparisonOperator.DEFINED, value)
+
+    def __eq__(self, value):
+        return self._build(ComparisonOperator.EQUAL_TO, value)
+
+    def __ne__(self, value):
+        return self._build(ComparisonOperator.NOT_EQUAL_TO, value)
+
+    def __gt__(self, value):
+        return self._build(ComparisonOperator.GREATER_THAN, value)
+
+    def __ge__(self, value):
+        return self._build(ComparisonOperator.GREATER_THAN_OR_EQUAL_TO, value)
+
+    def __lt__(self, value):
+        return self._build(ComparisonOperator.LESS_THAN, value)
+
+    def __le__(self, value):
+        return self._build(ComparisonOperator.LESS_THAN_OR_EQUAL_TO, value)
+
+    def one_of(self, values):
+        try:
+            first, remaining = values[0], values[1:]
+        except IndexError:
+            raise ValueError("Must provide at least one value")
+        filter = self == first
+        for val in remaining:
+            filter |= self == val
+        return filter
+
+
+class FilterBy(object):
+    @staticmethod
+    def experiment_id():
+        """Filter by experiment ID.
+
+        Examples
+        --------
+        Get runs for experiment 4:
+
+        >>> FilterBy.experiment_id() == 4
+        """
+        return _FilterBuilder(ExperimentIdFilter)
+
+    @staticmethod
+    def run_id():
+        """Filter by run ID.
+
+        Examples
+        --------
+        Get the run with a specified ID:
+
+        >>> FilterBy.run_id() == "945f1d96-9937-4b95-aa3f-addcdd1c8749"
+        """
+        return _FilterBuilder(RunIdFilter)
+
+    @staticmethod
+    def deleted_at():
+        """Filter by run deletion time.
+
+        Examples
+        --------
+        Get runs deleted more than ten minutes ago:
+
+        >>> from datetime import datetime, timedelta
+        >>> FilterBy.deleted_at() <  datetime.now() - timedelta(minutes=10)
+
+        Get non-deleted runs:
+
+        >>> FilterBy.deleted_at() == None
+        """
+        return _FilterBuilder(DeletedAtFilter)
+
+    @staticmethod
+    def tag(key):
+        """Filter by run tag.
+
+        Examples
+        --------
+        Get runs with a particular tag:
+
+        >>> FilterBy.tag("key") == "value"
+
+        Get runs where a tag is set, with any value:
+
+        >>> FilterBy.tag("key") != None
+        """
+        return _FilterBuilder(TagFilter, key)
+
+    @staticmethod
+    def param(key):
+        """Filter by parameter.
+
+        Examples
+        --------
+        Get runs with a particular parameter value:
+
+        >>> FilterBy.param("key") == "value"
+
+        Params also support filtering by numeric value:
+
+        >>> FilterBy.param("alpha") > 0.2
+        """
+        return _FilterBuilder(ParamFilter, key)
+
+    @staticmethod
+    def metric(key):
+        """Filter by metric.
+
+        Examples
+        --------
+        Get runs with matching metric values:
+
+        >>> FilterBy.metric("accuracy") > 0.9
+
+        To filter a range of values, combine them with ``&``:
+
+        >>> (
+        ...     (FilterBy.metric("accuracy") > 0.8 ) &
+        ...     (FilterBy.metric("accuracy") > 0.9)
+        ... )
+        """
+        return _FilterBuilder(MetricFilter, key)
--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -1,200 +1,298 @@
+# Copyright 2018-2019 Faculty Science Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 from datetime import datetime
 from uuid import uuid4
+import operator
 
 import pytest
 from pytz import UTC
+import mock
 
 from faculty.clients.experiment import (
     ComparisonOperator,
+    CompoundFilter,
+    DeletedAtFilter,
     ExperimentIdFilter,
-    Experiment,
-    ExperimentRun,
+    ExperimentRun as ClientExperimentRun,
     ExperimentRunStatus,
-    Metric,
-    MetricSort,
-    Param,
-    SortOrder,
-    Tag,
-)
-from faculty.clients.experiment._models import (
-    ListExperimentRunsResponse,
-    Page,
-    Pagination,
+    LogicalOperator,
+    MetricFilter,
+    ParamFilter,
+    RunIdFilter,
+    TagFilter,
 )
 
-from faculty.experiment import (
-    ExperimentRun as FacultyExperimentRun,
-    ExperimentRunQueryResult,
-)
+from faculty.experiment import ExperimentRun, ExperimentRunList, FilterBy
 
 
-PROJECT_ID = uuid4()
-EXPERIMENT_ID = 661
-EXPERIMENT_RUN_ID = uuid4()
-EXPERIMENT_RUN_NUMBER = 3
-EXPERIMENT_RUN_NAME = "run name"
-PARENT_RUN_ID = uuid4()
-RUN_STARTED_AT = datetime(2018, 3, 10, 11, 39, 12, 110000, tzinfo=UTC)
-RUN_ENDED_AT = datetime(2018, 3, 10, 11, 39, 15, 110000, tzinfo=UTC)
-CREATED_AT = datetime(2018, 3, 10, 11, 32, 6, 247000, tzinfo=UTC)
-LAST_UPDATED_AT = datetime(2018, 3, 10, 11, 32, 30, 172000, tzinfo=UTC)
-DELETED_AT = datetime(2018, 3, 10, 11, 37, 42, 482000, tzinfo=UTC)
-TAG = Tag(key="tag-key", value="tag-value")
-PARAM = Param(key="param-key", value="param-value")
-METRIC_KEY = "metric-key"
-METRIC_TIMESTAMP = datetime(2018, 3, 12, 16, 20, 22, 122000, tzinfo=UTC)
-METRIC = Metric(key=METRIC_KEY, step=1, value=123, timestamp=METRIC_TIMESTAMP)
+DATETIMES = [
+    datetime(2018, 3, 10, 11, 39, 12, 110000, tzinfo=UTC),
+    datetime(2018, 3, 10, 11, 32, 6, 247000, tzinfo=UTC),
+    datetime(2018, 3, 10, 11, 32, 30, 172000, tzinfo=UTC),
+    datetime(2018, 3, 10, 11, 37, 42, 482000, tzinfo=UTC),
+]
 
-EXPERIMENT = Experiment(
-    id=EXPERIMENT_ID,
-    name="experiment name",
-    description="experiment description",
-    artifact_location="https://example.com",
-    created_at=CREATED_AT,
-    last_updated_at=LAST_UPDATED_AT,
-    deleted_at=DELETED_AT,
-)
 
-FILTER = ExperimentIdFilter(ComparisonOperator.EQUAL_TO, 2)
-
-SORT = [MetricSort("metric-key", SortOrder.ASC)]
-
-EXPERIMENT_RUN = ExperimentRun(
-    id=EXPERIMENT_RUN_ID,
-    run_number=EXPERIMENT_RUN_NUMBER,
-    name=EXPERIMENT_RUN_NAME,
-    parent_run_id=PARENT_RUN_ID,
-    experiment_id=EXPERIMENT.id,
-    artifact_location="faculty:",
-    status=ExperimentRunStatus.RUNNING,
-    started_at=RUN_STARTED_AT,
-    ended_at=RUN_ENDED_AT,
-    deleted_at=DELETED_AT,
-    tags=[TAG],
-    params=[PARAM],
-    metrics=[METRIC],
-)
-PAGINATION = Pagination(0, 1, None, None)
-LIST_EXPERIMENT_RUNS_RESPONSE = ListExperimentRunsResponse(
-    runs=[EXPERIMENT_RUN], pagination=PAGINATION
-)
-EXPECTED_RUNS = [
-    FacultyExperimentRun(
-        id=EXPERIMENT_RUN_ID,
-        run_number=EXPERIMENT_RUN_NUMBER,
-        name=EXPERIMENT_RUN_NAME,
-        parent_run_id=PARENT_RUN_ID,
-        experiment_id=EXPERIMENT.id,
-        artifact_location="faculty:",
-        status=ExperimentRunStatus.RUNNING,
-        started_at=RUN_STARTED_AT,
-        ended_at=RUN_ENDED_AT,
-        deleted_at=DELETED_AT,
-        tags=[TAG],
-        params=[PARAM],
-        metrics=[METRIC],
+def mock_client_run():
+    return ClientExperimentRun(
+        **{field: mock.Mock() for field in ClientExperimentRun._fields}
     )
-]
-
-PAGINATION_MULTIPLE_1 = Pagination(0, 1, None, Page(1, 1))
-LIST_EXPERIMENT_RUNS_RESPONSE_MULTIPLE_1 = ListExperimentRunsResponse(
-    runs=[EXPERIMENT_RUN], pagination=PAGINATION_MULTIPLE_1
-)
-EXPERIMENT_RUN_MULTIPLE_2 = ExperimentRun(
-    id=7,
-    run_number=EXPERIMENT_RUN_NUMBER,
-    name=EXPERIMENT_RUN_NAME,
-    parent_run_id=PARENT_RUN_ID,
-    experiment_id=EXPERIMENT.id,
-    artifact_location="faculty:",
-    status=ExperimentRunStatus.RUNNING,
-    started_at=RUN_STARTED_AT,
-    ended_at=RUN_ENDED_AT,
-    deleted_at=DELETED_AT,
-    tags=[TAG],
-    params=[PARAM],
-    metrics=[METRIC],
-)
-PAGINATION_MULTIPLE_2 = Pagination(1, 1, Page(0, 1), None)
-LIST_EXPERIMENT_RUNS_RESPONSE_MULTIPLE_2 = ListExperimentRunsResponse(
-    runs=[EXPERIMENT_RUN_MULTIPLE_2], pagination=PAGINATION_MULTIPLE_2
-)
-EXPECTED_RUNS_2 = [
-    FacultyExperimentRun(
-        id=EXPERIMENT_RUN_ID,
-        run_number=EXPERIMENT_RUN_NUMBER,
-        name=EXPERIMENT_RUN_NAME,
-        parent_run_id=PARENT_RUN_ID,
-        experiment_id=EXPERIMENT.id,
-        artifact_location="faculty:",
-        status=ExperimentRunStatus.RUNNING,
-        started_at=RUN_STARTED_AT,
-        ended_at=RUN_ENDED_AT,
-        deleted_at=DELETED_AT,
-        tags=[TAG],
-        params=[PARAM],
-        metrics=[METRIC],
-    ),
-    FacultyExperimentRun(
-        id=7,
-        run_number=EXPERIMENT_RUN_NUMBER,
-        name=EXPERIMENT_RUN_NAME,
-        parent_run_id=PARENT_RUN_ID,
-        experiment_id=EXPERIMENT.id,
-        artifact_location="faculty:",
-        status=ExperimentRunStatus.RUNNING,
-        started_at=RUN_STARTED_AT,
-        ended_at=RUN_ENDED_AT,
-        deleted_at=DELETED_AT,
-        tags=[TAG],
-        params=[PARAM],
-        metrics=[METRIC],
-    ),
-]
 
 
-@pytest.mark.parametrize(
-    "query_runs_side_effects,expected_runs",
-    [
-        [[LIST_EXPERIMENT_RUNS_RESPONSE], EXPECTED_RUNS],
+def expected_resource_run_for_client_run(run):
+    return ExperimentRun(
+        id=run.id,
+        run_number=run.run_number,
+        experiment_id=run.experiment_id,
+        name=run.name,
+        parent_run_id=run.parent_run_id,
+        artifact_location=run.artifact_location,
+        status=run.status,
+        started_at=run.started_at,
+        ended_at=run.ended_at,
+        deleted_at=run.deleted_at,
+        tags=run.tags,
+        params=run.params,
+        metrics=run.metrics,
+    )
+
+
+def test_experiment_run_query(mocker):
+    session = mocker.Mock()
+    get_session_mock = mocker.patch(
+        "faculty.experiment.get_session", return_value=session
+    )
+
+    project_id = mocker.Mock()
+    resolve_project_id_mock = mocker.patch(
+        "faculty.experiment.resolve_project_id", return_value=project_id
+    )
+
+    client = mocker.Mock()
+    mocker.patch("faculty.experiment.ExperimentClient", return_value=client)
+    client_runs = [mock_client_run(), mock_client_run()]
+    client_response = mocker.Mock()
+    client_response.runs = client_runs
+    client_response.pagination.next = None
+    client.query_runs.return_value = client_response
+
+    filter = mocker.Mock()
+    sort = mocker.Mock()
+
+    runs = ExperimentRun.query("my project", filter, sort, extra_conf="foo")
+    assert runs == ExperimentRunList(
+        [expected_resource_run_for_client_run(run) for run in client_runs]
+    )
+
+    get_session_mock.assert_called_once_with(extra_conf="foo")
+    resolve_project_id_mock.assert_called_once_with(session, "my project")
+    client.query_runs.assert_called_once_with(project_id, filter, sort)
+
+
+def test_experiment_run_query_multiple_pages(mocker):
+    session = mocker.Mock()
+    get_session_mock = mocker.patch(
+        "faculty.experiment.get_session", return_value=session
+    )
+
+    project_id = mocker.Mock()
+    resolve_project_id_mock = mocker.patch(
+        "faculty.experiment.resolve_project_id", return_value=project_id
+    )
+
+    client = mocker.Mock()
+    mocker.patch("faculty.experiment.ExperimentClient", return_value=client)
+    client_response_0 = mocker.Mock()
+    client_response_0.runs = [mock_client_run(), mock_client_run()]
+    client_response_1 = mocker.Mock()
+    client_response_1.runs = [mock_client_run(), mock_client_run()]
+    client_response_2 = mocker.Mock()
+    client_response_2.runs = [mock_client_run(), mock_client_run()]
+    client_response_2.pagination.next = None
+    client.query_runs.side_effect = [
+        client_response_0,
+        client_response_1,
+        client_response_2,
+    ]
+    all_client_runs = (
+        client_response_0.runs
+        + client_response_1.runs
+        + client_response_2.runs
+    )
+
+    filter = mocker.Mock()
+    sort = mocker.Mock()
+
+    runs = ExperimentRun.query("my project", filter, sort, extra_conf="foo")
+    assert runs == ExperimentRunList(
+        [expected_resource_run_for_client_run(run) for run in all_client_runs]
+    )
+
+    get_session_mock.assert_called_once_with(extra_conf="foo")
+    resolve_project_id_mock.assert_called_once_with(session, "my project")
+    client.query_runs.assert_has_calls(
         [
-            [
-                LIST_EXPERIMENT_RUNS_RESPONSE_MULTIPLE_1,
-                LIST_EXPERIMENT_RUNS_RESPONSE_MULTIPLE_2,
-            ],
-            EXPECTED_RUNS_2,
+            mocker.call(project_id, filter, sort),
+            mocker.call(
+                project_id,
+                filter,
+                sort,
+                start=client_response_0.pagination.next.start,
+                limit=client_response_0.pagination.next.limit,
+            ),
+            mocker.call(
+                project_id,
+                filter,
+                sort,
+                start=client_response_1.pagination.next.start,
+                limit=client_response_1.pagination.next.limit,
+            ),
+        ]
+    )
+
+
+def test_experiment_run_list_as_dataframe(mocker):
+    run_0 = mocker.Mock(
+        experiment_id=1,
+        id=uuid4(),
+        run_number=3,
+        status=ExperimentRunStatus.FINISHED,
+        started_at=DATETIMES[0],
+        ended_at=DATETIMES[1],
+        params=[
+            mocker.Mock(key="classic", value="foo"),
+            mocker.Mock(key="monty", value="spam"),
         ],
-    ],
-)
-def test_experiment_run_query_single_call(
-    mocker, query_runs_side_effects, expected_runs
+        metrics=[
+            mocker.Mock(key="accuracy", value=0.87),
+            mocker.Mock(key="f1_score", value=0.76),
+        ],
+    )
+    run_1 = mocker.Mock(
+        experiment_id=2,
+        id=uuid4(),
+        run_number=4,
+        status=ExperimentRunStatus.RUNNING,
+        started_at=DATETIMES[2],
+        ended_at=DATETIMES[3],
+        params=[
+            mocker.Mock(key="classic", value="bar"),
+            mocker.Mock(key="monty", value="eggs"),
+        ],
+        metrics=[
+            mocker.Mock(key="accuracy", value=0.91),
+            mocker.Mock(key="f1_score", value=0.72),
+        ],
+    )
+
+    runs_df = ExperimentRunList([run_0, run_1]).as_dataframe()
+
+    assert list(runs_df.columns) == [
+        ("experiment_id", ""),
+        ("run_id", ""),
+        ("run_number", ""),
+        ("status", ""),
+        ("started_at", ""),
+        ("ended_at", ""),
+        ("params", "classic"),
+        ("params", "monty"),
+        ("metrics", "accuracy"),
+        ("metrics", "f1_score"),
+    ]
+    assert (runs_df.experiment_id == [1, 2]).all()
+    assert (runs_df.run_id == [run_0.id, run_1.id]).all()
+    assert (runs_df.run_number == [3, 4]).all()
+    assert (runs_df.status == ["finished", "running"]).all()
+    assert (runs_df.started_at == [DATETIMES[0], DATETIMES[2]]).all()
+    assert (runs_df.ended_at == [DATETIMES[1], DATETIMES[3]]).all()
+    assert (runs_df.params.classic == ["foo", "bar"]).all()
+    assert (runs_df.params.monty == ["spam", "eggs"]).all()
+    assert (runs_df.metrics.accuracy == [0.87, 0.91]).all()
+    assert (runs_df.metrics.f1_score == [0.76, 0.72]).all()
+
+
+FILTER_BY_NO_KEY_CASES = [
+    (FilterBy.experiment_id, ExperimentIdFilter),
+    (FilterBy.run_id, RunIdFilter),
+    (FilterBy.deleted_at, DeletedAtFilter),
+]
+FILTER_BY_WITH_KEY_CASES = [
+    (FilterBy.tag, TagFilter),
+    (FilterBy.param, ParamFilter),
+    (FilterBy.metric, MetricFilter),
+]
+
+OPERATOR_CASES = [
+    (lambda x, v: x.defined(v), ComparisonOperator.DEFINED),
+    (operator.eq, ComparisonOperator.EQUAL_TO),
+    (operator.ne, ComparisonOperator.NOT_EQUAL_TO),
+    (operator.gt, ComparisonOperator.GREATER_THAN),
+    (operator.ge, ComparisonOperator.GREATER_THAN_OR_EQUAL_TO),
+    (operator.lt, ComparisonOperator.LESS_THAN),
+    (operator.le, ComparisonOperator.LESS_THAN_OR_EQUAL_TO),
+]
+
+
+@pytest.mark.parametrize("method, filter_cls", FILTER_BY_NO_KEY_CASES)
+@pytest.mark.parametrize("python_operator, expected_operator", OPERATOR_CASES)
+def test_filter_by_no_key(
+    mocker, method, filter_cls, python_operator, expected_operator
 ):
-    experiment_client_mock = mocker.MagicMock()
-    experiment_client_mock.query_runs = mocker.MagicMock(
-        side_effect=query_runs_side_effects
+    value = mocker.Mock()
+    filter = python_operator(method(), value)
+    expected = filter_cls(expected_operator, value)
+    assert filter == expected
+
+
+@pytest.mark.parametrize("method, filter_cls", FILTER_BY_WITH_KEY_CASES)
+@pytest.mark.parametrize("python_operator, expected_operator", OPERATOR_CASES)
+def test_filter_by_with_key(
+    mocker, method, filter_cls, python_operator, expected_operator
+):
+    key = mocker.Mock()
+    value = mocker.Mock()
+    filter = python_operator(method(key), value)
+    expected = filter_cls(key, expected_operator, value)
+    assert filter == expected
+
+
+@pytest.mark.parametrize("method, filter_cls", FILTER_BY_NO_KEY_CASES)
+def test_filter_by_one_of_no_key(mocker, method, filter_cls):
+    values = [mocker.Mock(), mocker.Mock()]
+    filter = method().one_of(values)
+    expected = CompoundFilter(
+        LogicalOperator.OR,
+        [
+            filter_cls(ComparisonOperator.EQUAL_TO, values[0]),
+            filter_cls(ComparisonOperator.EQUAL_TO, values[1]),
+        ],
     )
-    mocker.patch("faculty.client", return_value=experiment_client_mock)
-
-    response = FacultyExperimentRun.query(PROJECT_ID, FILTER, SORT)
-
-    assert isinstance(response, ExperimentRunQueryResult)
-    returned_runs = list(response)
-    for expected_run, returned_run in zip(expected_runs, returned_runs):
-        assert isinstance(returned_run, FacultyExperimentRun)
-        assert _are_runs_equal(expected_run, returned_run)
+    assert filter == expected
 
 
-def _are_runs_equal(this, that):
-    return all(
-        list(
-            i == j
-            for i, j in (
-                list(
-                    zip(
-                        [getattr(this, attr) for attr in this.__dict__.keys()],
-                        [getattr(that, attr) for attr in that.__dict__.keys()],
-                    )
-                )
-            )
-        )
+@pytest.mark.parametrize("method, filter_cls", FILTER_BY_WITH_KEY_CASES)
+def test_filter_by_one_of_with_key(mocker, method, filter_cls):
+    key = mocker.Mock()
+    values = [mocker.Mock(), mocker.Mock()]
+    filter = method(key).one_of(values)
+    expected = CompoundFilter(
+        LogicalOperator.OR,
+        [
+            filter_cls(key, ComparisonOperator.EQUAL_TO, values[0]),
+            filter_cls(key, ComparisonOperator.EQUAL_TO, values[1]),
+        ],
     )
+    assert filter == expected
