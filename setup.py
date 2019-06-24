c9f37a7a05a7049c6d958d49a94c3e819b025298
Pascal Bugnion <pascal@bugnion.org>
2019-06-24 18:57:40 +0100

    Bump to latest version of marshmallow to support Python2

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         # Install marshmallow with 'reco' (recommended) extras to ensure a
         # compatible version of python-dateutil is available
         "attrs",
-        "marshmallow[reco]==3.0.0rc3",
+        "marshmallow[reco]==3.0.0rc5",
         "marshmallow_enum",
         "boto3",
         "botocore",
