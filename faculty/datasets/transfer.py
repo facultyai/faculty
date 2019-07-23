1487f9a8dff1cc36650c82bb9820f913c2f2ec70
Andrew Crozier <wacrozier@gmail.com>
2019-07-23 15:13:31 +0100

    Extend transfer module to support reading/writing data from memory

--- faculty/datasets/transfer.py
+++ faculty/datasets/transfer.py
@@ -22,8 +22,8 @@ from faculty.datasets.session import DatasetsError
 MEGABYTE = 1024 * 1024
 
 
-def download_file(object_client, project_id, datasets_path, local_path):
-    """Download a file from the object store.
+def download(object_client, project_id, datasets_path):
+    """Download the contents of file from the object store.
 
     Parameters
     ----------
@@ -31,8 +31,34 @@ def download_file(object_client, project_id, datasets_path, local_path):
     project_id : uuid.UUID
     datasets_path : str
         The target path to upload to in the object store
-    local_path : str
-        The local path of the object to upload
+
+    Returns
+    -------
+    bytes
+        The content of the file
+    """
+
+    content = b""
+    for chunk in download_stream(object_client, project_id, datasets_path):
+        content += chunk
+
+    return content
+
+
+def download_stream(object_client, project_id, datasets_path):
+    """Stream the contents of file from the object store.
+
+    Parameters
+    ----------
+    object_client : faculty.clients.object.ObjectClient
+    project_id : uuid.UUID
+    datasets_path : str
+        The target path to upload to in the object store
+
+    Returns
+    -------
+    Iterable[bytes]
+        The content of the file, chunked
     """
 
     url = object_client.presign_download(project_id, datasets_path)
@@ -48,10 +74,80 @@ def download_file(object_client, project_id, datasets_path, local_path):
 
         response.raise_for_status()
 
-        with open(local_path, "wb") as fp:
-            for chunk in response.iter_content():
-                if chunk:  # Filter out keep-alive chunks
-                    fp.write(chunk)
+        for chunk in response.iter_content():
+            if chunk:  # Filter out keep-alive chunks
+                yield chunk
+
+
+def download_file(object_client, project_id, datasets_path, local_path):
+    """Download a file from the object store.
+
+    Parameters
+    ----------
+    object_client : faculty.clients.object.ObjectClient
+    project_id : uuid.UUID
+    datasets_path : str
+        The target path to upload to in the object store
+    local_path : str
+        The local path of the object to upload
+    """
+
+    # Initiate the download to allow any failures to happen before opening the
+    # file
+    stream = download(object_client, project_id, datasets_path)
+
+    with open(local_path, "wb") as fp:
+        for chunk in stream:
+            fp.write(chunk)
+
+
+def upload(object_client, project_id, datasets_path, content):
+    """Upload data to the object store.
+
+    Parameters
+    ----------
+    object_client : faculty.clients.object.ObjectClient
+    project_id : uuid.UUID
+    datasets_path : str
+        The target path to upload to in the object store
+    content : bytes
+        The data to upload
+    """
+    # upload_stream will rechunk the data anyway so just pass as a single chunk
+    upload_stream(object_client, project_id, datasets_path, [content])
+
+
+def upload_stream(object_client, project_id, datasets_path, content):
+    """Upload data to the object store from an iterable.
+
+    Parameters
+    ----------
+    object_client : faculty.clients.object.ObjectClient
+    project_id : uuid.UUID
+    datasets_path : str
+        The target path to upload to in the object store
+    content : Iterable[bytes]
+        The data to upload, chunked
+    """
+
+    presign_response = object_client.presign_upload(project_id, datasets_path)
+
+    if presign_response.provider == CloudStorageProvider.S3:
+        _s3_upload(
+            object_client,
+            project_id,
+            datasets_path,
+            content,
+            presign_response.upload_id,
+        )
+    elif presign_response.provider == CloudStorageProvider.GCS:
+        _gcs_upload(presign_response.url, content)
+    else:
+        raise ValueError(
+            "Unsupported cloud storage provider: {}".format(
+                presign_response.provider
+            )
+        )
 
 
 def upload_file(object_client, project_id, datasets_path, local_path):
@@ -66,42 +162,19 @@ def upload_file(object_client, project_id, datasets_path, local_path):
     local_path : str
         The local path of the object to upload
     """
-
-    presign_response = object_client.presign_upload(project_id, datasets_path)
-
-    if presign_response.provider == CloudStorageProvider.S3:
-        _s3_upload(
-            object_client,
-            project_id,
-            datasets_path,
-            local_path,
-            presign_response.upload_id,
-        )
-    elif presign_response.provider == CloudStorageProvider.GCS:
-        _gcs_upload(presign_response.url, local_path)
-    else:
-        raise ValueError(
-            "Unsupported cloud storage provider: {}".format(
-                presign_response.provider
-            )
-        )
+    upload_stream(
+        object_client,
+        project_id,
+        datasets_path,
+        _file_chunk_iterator(local_path),
+    )
 
 
-def _file_chunk_iterator(local_path, chunk_size=5 * MEGABYTE):
-    with open(local_path, "rb") as fp:
-        chunk = fp.read(chunk_size)
-        while chunk:
-            yield chunk
-            chunk = fp.read(chunk_size)
-
-
-def _s3_upload(
-    object_client, project_id, datasets_path, local_path, upload_id
-):
+def _s3_upload(object_client, project_id, datasets_path, content, upload_id):
 
     completed_parts = []
 
-    for i, chunk in enumerate(_file_chunk_iterator(local_path)):
+    for i, chunk in enumerate(_rechunk_data(content)):
 
         part_number = i + 1
 
@@ -123,5 +196,33 @@ def _s3_upload(
     )
 
 
-def _gcs_upload(upload_url, local_path):
+def _gcs_upload(upload_url, content):
     raise NotImplementedError("GCS upload is not implemented")
+
+
+def _file_chunk_iterator(local_path, chunk_size=5 * MEGABYTE):
+    with open(local_path, "rb") as fp:
+        chunk = fp.read(chunk_size)
+        while chunk:
+            yield chunk
+            chunk = fp.read(chunk_size)
+
+
+def _rechunk_data(content, chunk_size=5 * MEGABYTE):
+
+    chunk = b""
+
+    for original_chunk in content:
+
+        while len(original_chunk) > 0:
+
+            remaining = chunk_size - len(chunk)
+            chunk += original_chunk[:remaining]
+            original_chunk = original_chunk[remaining:]
+
+            if len(chunk) >= chunk_size:
+                yield chunk
+                chunk = b""
+
+    if len(chunk) > 0:
+        yield chunk
