a07f217589ccefa722b3a58bf5332b1415a61c26
Andrew Crozier <wacrozier@gmail.com>
2019-07-24 15:49:34 +0100

    Factor out common fixtures

--- tests/datasets/test_transfer.py
+++ tests/datasets/test_transfer.py
@@ -17,6 +17,8 @@ from uuid import uuid4
 import random
 import string
 
+import pytest
+
 from faculty.datasets import transfer
 
 
@@ -29,49 +31,38 @@ TEST_CONTENT = "".join(
 ).encode("utf8")
 
 
-def test_download(mocker, requests_mock):
-
+@pytest.fixture
+def mock_client_download(mocker, requests_mock):
     object_client = mocker.Mock()
     object_client.presign_download.return_value = TEST_URL
 
     requests_mock.get(TEST_URL, content=TEST_CONTENT)
 
-    assert (
-        transfer.download(object_client, PROJECT_ID, TEST_PATH) == TEST_CONTENT
-    )
+    yield object_client
 
     object_client.presign_download.assert_called_once_with(
         PROJECT_ID, TEST_PATH
     )
 
 
-def test_download_stream(mocker, requests_mock):
+def test_download(mock_client_download):
+    assert (
+        transfer.download(mock_client_download, PROJECT_ID, TEST_PATH)
+        == TEST_CONTENT
+    )
 
-    object_client = mocker.Mock()
-    object_client.presign_download.return_value = TEST_URL
 
-    requests_mock.get(TEST_URL, content=TEST_CONTENT)
-
-    stream = transfer.download_stream(object_client, PROJECT_ID, TEST_PATH)
+def test_download_stream(mock_client_download):
+    stream = transfer.download_stream(
+        mock_client_download, PROJECT_ID, TEST_PATH
+    )
     assert b"".join(stream) == TEST_CONTENT
 
-    object_client.presign_download.assert_called_once_with(
-        PROJECT_ID, TEST_PATH
-    )
 
-
-def test_download_file(mocker, requests_mock, tmpdir):
-
-    object_client = mocker.Mock()
-    object_client.presign_download.return_value = TEST_URL
-
-    requests_mock.get(TEST_URL, content=TEST_CONTENT)
-
+def test_download_file(mock_client_download, tmpdir):
     destination = tmpdir.join("destination.txt")
 
-    transfer.download_file(object_client, PROJECT_ID, TEST_PATH, destination)
-    assert destination.read(mode="rb") == TEST_CONTENT
-
-    object_client.presign_download.assert_called_once_with(
-        PROJECT_ID, TEST_PATH
+    transfer.download_file(
+        mock_client_download, PROJECT_ID, TEST_PATH, destination
     )
+    assert destination.read(mode="rb") == TEST_CONTENT
