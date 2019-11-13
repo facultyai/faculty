8d36d6a9b8703a6f6dc34f07c478fe0fd7fd34e1
Maitiu <maitiu@faculty.ai>
2019-11-13 16:57:55 +0000

    Make desciption field nullable

--- faculty/clients/environment.py
+++ faculty/clients/environment.py
@@ -233,7 +233,7 @@ class EnvironmentSchema(BaseSchema):
     id = fields.UUID(data_key="environmentId", required=True)
     project_id = fields.UUID(data_key="projectId", required=True)
     name = fields.String(required=True)
-    description = fields.String(required=True)
+    description = fields.String(required=True, allow_none=True)
     author_id = fields.UUID(data_key="authorId", required=True)
     created_at = fields.DateTime(data_key="createdAt", required=True)
     updated_at = fields.DateTime(data_key="updatedAt", required=True)
