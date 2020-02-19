f1fb9139b9fb7259c2edf140b656ad786a4962bb
Andrew Crozier <wacrozier@gmail.com>
2020-02-19 16:12:09 +0000

    Improve documentation of cluster module and mark non-user-facing classes as private

--- faculty/clients/cluster.py
+++ faculty/clients/cluster.py
@@ -41,31 +41,19 @@ NodeType = namedtuple(
 )
 
 
-class NodeTypeSchema(BaseSchema):
-
-    id = fields.String(data_key="nodeTypeId", required=True)
-    name = fields.String(missing=None)
-    instance_group = fields.String(data_key="instanceGroup", missing=None)
-    max_interactive_instances = fields.Integer(
-        data_key="maxInteractiveInstances", required=True
-    )
-    max_job_instances = fields.Integer(
-        data_key="maxJobInstances", required=True
-    )
-    milli_cpus = fields.Integer(data_key="milliCpus", required=True)
-    memory_mb = fields.Integer(data_key="memoryMb", required=True)
-    num_gpus = fields.Integer(data_key="numGpus", required=True)
-    gpu_name = fields.String(data_key="gpuName", missing=None)
-    cost_usd_per_hour = fields.Decimal(
-        data_key="costUsdPerHour", required=True
-    )
-
-    @post_load
-    def make_node_type(self, data):
-        return NodeType(**data)
-
-
 class ClusterClient(BaseClient):
+    """Client for the Faculty cluster configuration service.
+
+    Either build this client with a session directly, or use the
+    :func:`faculty.client` helper function:
+
+    >>> client = faculty.client("cluster")
+
+    Parameters
+    ----------
+    session : faculty.session.Session
+        The session to use to make requests
+    """
 
     _SERVICE_NAME = "klostermann"
 
@@ -104,7 +92,7 @@ class ClusterClient(BaseClient):
 
         return self._get(
             "/node-type/single-tenanted",
-            NodeTypeSchema(many=True),
+            _NodeTypeSchema(many=True),
             params=query_params,
         )
 
@@ -116,6 +104,28 @@ class ClusterClient(BaseClient):
         max_interactive_instances,
         max_job_instances,
     ):
+        """Configure a single tenanted node type on the cluster.
+
+        If this node type is already configured, this will update the
+        configuration, otherwise it will be added to those available.
+
+        Parameters
+        ----------
+        node_type_id : str
+            The ID of the node type to configure, e.g. m4.xlarge on AWS or
+            n1-standard-4 on GCP.
+        name : str
+            A name for this node type. Usually the same as the node type ID.
+        instance_group : str
+            The Kubernetes instance group that provides this node type. The
+            instance group must already exist.
+        max_interactive_instances : int
+            The maximum number of interactive servers (Jupyter, RStudio, apps,
+            APIs) using this node type to allow to run simultaneously.
+        max_job_instances : int
+            The maximum number of jobs runs using this node type to allow to
+            run simultaneously.
+        """
         payload = {
             "name": name,
             "instanceGroup": instance_group,
@@ -128,6 +138,38 @@ class ClusterClient(BaseClient):
         )
 
     def disable_single_tenanted_node_type(self, node_type_id):
+        """Disable a single tenanted node type on the cluster.
+
+        Parameters
+        ----------
+        node_type_id : str
+            The ID of the node type to disable, e.g. m4.xlarge on AWS or
+            n1-standard-4 on GCP.
+        """
         self._delete_raw(
             "/node-type/single-tenanted/{}/configuration".format(node_type_id)
         )
+
+
+class _NodeTypeSchema(BaseSchema):
+
+    id = fields.String(data_key="nodeTypeId", required=True)
+    name = fields.String(missing=None)
+    instance_group = fields.String(data_key="instanceGroup", missing=None)
+    max_interactive_instances = fields.Integer(
+        data_key="maxInteractiveInstances", required=True
+    )
+    max_job_instances = fields.Integer(
+        data_key="maxJobInstances", required=True
+    )
+    milli_cpus = fields.Integer(data_key="milliCpus", required=True)
+    memory_mb = fields.Integer(data_key="memoryMb", required=True)
+    num_gpus = fields.Integer(data_key="numGpus", required=True)
+    gpu_name = fields.String(data_key="gpuName", missing=None)
+    cost_usd_per_hour = fields.Decimal(
+        data_key="costUsdPerHour", required=True
+    )
+
+    @post_load
+    def make_node_type(self, data):
+        return NodeType(**data)
