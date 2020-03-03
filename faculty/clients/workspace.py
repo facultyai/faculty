7e497e0069ad37d3946ac1d29d21ff3ae6794268
Andrew Crozier <wacrozier@gmail.com>
2020-03-03 18:55:28 +0000

    Improve documentation of workspace module and mark non-user-facing classes as private

--- faculty/clients/workspace.py
+++ faculty/clients/workspace.py
@@ -26,24 +26,65 @@ from marshmallow_enum import EnumField
 from faculty.clients.base import BaseSchema, BaseClient
 
 
-class FileNodeType(Enum):
-    FILE = "file"
-    DIRECTORY = "directory"
-
-
 File = namedtuple("File", ["path", "name", "last_modified", "size"])
 Directory = namedtuple(
     "Directory",
     ["path", "name", "last_modified", "size", "truncated", "content"],
 )
-ListResponse = namedtuple("ListResponse", ["project_id", "path", "content"])
 
 
-class FileNodeSchema(BaseSchema):
+class WorkspaceClient(BaseClient):
+    """Client for the Faculty workspace service.
+
+    Either build this client with a session directly, or use the
+    :func:`faculty.client` helper function:
+
+    >>> client = faculty.client("workspace")
+
+    Parameters
+    ----------
+    session : faculty.session.Session
+        The session to use to make requests
+    """
+
+    _SERVICE_NAME = "workspace"
+
+    def list(self, project_id, prefix, depth):
+        """List files in a project workspace.
+
+        Parameters
+        ----------
+        project_id : uuid.UUID
+            The ID of the project to list files in.
+        prefix : str
+            A file prefix to list files under.
+        depth : int
+            How deep down the file tree to list.
+
+        Returns
+        -------
+        Union[File, Directory]
+            The listed file tree.
+        """
+        endpoint = "/project/{}/file".format(project_id)
+        params = {"depth": depth, "prefix": prefix}
+        response = self._get(endpoint, _ListResponseSchema(), params=params)
+        return response.content
+
+
+class _FileNodeType(Enum):
+    FILE = "file"
+    DIRECTORY = "directory"
+
+
+_ListResponse = namedtuple("_ListResponse", ["project_id", "path", "content"])
+
+
+class _FileNodeSchema(BaseSchema):
 
     path = fields.String(required=True)
     name = fields.String(required=True)
-    type = EnumField(FileNodeType, by_value=True, required=True)
+    type = EnumField(_FileNodeType, by_value=True, required=True)
     last_modified = fields.DateTime(required=True)
     size = fields.Integer(required=True)
     truncated = fields.Boolean()
@@ -51,40 +92,29 @@ class FileNodeSchema(BaseSchema):
 
     @validates_schema
     def validate_type(self, data):
-        if data["type"] == FileNodeType.DIRECTORY:
+        if data["type"] == _FileNodeType.DIRECTORY:
             required_fields = Directory._fields
-        elif data["type"] == FileNodeType.FILE:
+        elif data["type"] == _FileNodeType.FILE:
             required_fields = File._fields
         if set(data.keys()) != set(required_fields).union({"type"}):
             raise ValidationError("Wrong fields for {}.".format(data["type"]))
 
     @post_load
     def make_file_node(self, data):
-        if data["type"] == FileNodeType.DIRECTORY:
+        if data["type"] == _FileNodeType.DIRECTORY:
             return Directory(**{key: data[key] for key in Directory._fields})
-        elif data["type"] == FileNodeType.FILE:
+        elif data["type"] == _FileNodeType.FILE:
             return File(**{key: data[key] for key in File._fields})
         else:
             raise ValueError("Invalid file node type.")
 
 
-class ListResponseSchema(BaseSchema):
+class _ListResponseSchema(BaseSchema):
 
     project_id = fields.UUID(data_key="project_id", required=True)
     path = fields.String(required=True)
-    content = fields.List(fields.Nested(FileNodeSchema), required=True)
+    content = fields.List(fields.Nested(_FileNodeSchema), required=True)
 
     @post_load
     def make_list_response(self, data):
-        return ListResponse(**data)
-
-
-class WorkspaceClient(BaseClient):
-
-    _SERVICE_NAME = "workspace"
-
-    def list(self, project_id, prefix, depth):
-        endpoint = "/project/{}/file".format(project_id)
-        params = {"depth": depth, "prefix": prefix}
-        response = self._get(endpoint, ListResponseSchema(), params=params)
-        return response.content
+        return _ListResponse(**data)
