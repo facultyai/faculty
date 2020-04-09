0d13e6520eff4f0b7e675f1b0f8ea6c256645c4d
Andrew Crozier <wacrozier@gmail.com>
2020-04-09 18:28:25 +0100

    Remove unused options

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,14 +32,6 @@ autodoc_default_options = {"members": True, "undoc-members": True}
 autodoc_member_order = "bysource"
 autosummary_generate = True
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -55,11 +47,6 @@ if "COMBINED_DOCS_ROOT" in os.environ:
 
 # Disable Sphinx attribution
 html_show_sphinx = False
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["static"]
 
 
 # -- Intersphinx config ------------------------------------------------------
