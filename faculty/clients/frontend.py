12d3184aa5284bed88ba885d953384bd0e176740
 Tomáš Milata <tomas@faculty.ai>
2020-09-02 11:57:34 +0000

    Simplify frontend client by using internal DNS

--- faculty/clients/frontend.py
+++ faculty/clients/frontend.py
@@ -30,20 +30,6 @@ class FrontendClient(BaseClient):
 
     _SERVICE_NAME = "frontend"
 
-    # TODO this is a copy-paste modificiation from BaseClient
-    def _request(self, method, endpoint, check_status=True, *args, **kwargs):
-        """Perform an HTTP request.
-
-        This method should not be called from subclasses directly. Instead,
-        call one of the HTTP verb-specific methods. If it does not exist yet
-        for the HTTP method you need, contribute it.
-        """
-        url = "https://gollum.platform.asidata.science/" + endpoint 
-        response = self.http_session.request(method, url, *args, **kwargs)
-        if check_status:
-            _check_status(response)
-        return response
-
     def user_updates(self, user_id):
         endpoint = "api/updates/user/{}".format(user_id)
         response = self._get_raw(endpoint, stream=True)
