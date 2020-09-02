d271b4b13fe3c6ce8693be8a8fbf33ae6f9e0714
 Tomáš Milata <tomas@faculty.ai>
2020-09-02 10:33:23 +0000

    Clean up unused code

--- faculty/clients/frontend.py
+++ faculty/clients/frontend.py
@@ -23,92 +23,30 @@ import requests
 import sseclient
 
 from faculty.clients.auth import FacultyAuth
-from faculty.clients.base import BaseClient
+from faculty.clients.base import _check_status, BaseClient
 import faculty.session
 
 class FrontendClient(BaseClient):
 
     _SERVICE_NAME = "frontend"
 
-    # def __init__(self, session):
-    #     self.session = session
+    # TODO this is a copy-paste modificiation from BaseClient
+    def _request(self, method, endpoint, check_status=True, *args, **kwargs):
+        """Perform an HTTP request.
 
-    def foo(self, user_id):
+        This method should not be called from subclasses directly. Instead,
+        call one of the HTTP verb-specific methods. If it does not exist yet
+        for the HTTP method you need, contribute it.
+        """
+        url = "https://gollum.platform.asidata.science/" + endpoint 
+        response = self.http_session.request(method, url, *args, **kwargs)
+        if check_status:
+            _check_status(response)
+        return response
+
+    def user_updates(self, user_id):
         endpoint = "api/updates/user/{}".format(user_id)
-        # endpoint = "https://gollum.platform.asidata.science/api/updates/user/{}".format(user_id)
-        # self._stream(endpoint)
-        # with self._stream(endpoint) as stream:
-        #     print("open")
-        #     for sse in stream:
-        #         print(sse.data)
-        response = requests.get("https://gollum.platform.asidata.science/api/updates/user/07a34d80-d386-4d27-9a46-c2504cba2fc6",headers={"Authorization": "Bearer ???"}, stream=True)
+        response = self._get_raw(endpoint, stream=True)
+        
         client = sseclient.SSEClient(response)
         return client.events()
-
-    # TODO move from faculty-cli
-    @contextmanager 
-    def _stream(self, endpoint):
-        """Stream from a SSE endpoint.
-
-        Usage
-        -----
-
-        >>> with self._stream(endpoint) as stream:
-        ...     for sse in stream:
-        ...         print(sse.data)
-
-        """
-        # auth = FacultyAuth(self.session)
-        # print(f"streaming endpoint {endpoint}")
-        response = requests.get("https://gollum.platform.asidata.science/api/updates/user/07a34d80-d386-4d27-9a46-c2504cba2fc6",headers={"Authorization": "Bearer ???"}, stream=True)
-        # response = self._get_raw(endpoint, stream=True)
-        print("sse_stream_iter")
-        def sse_stream_iterator():
-            buf = []
-            for line in response.iter_lines(decode_unicode=True):
-                print(f"line: {line}.")
-                if not line.strip():
-                    yield ServerSentEventMessage.from_lines(buf)
-                    buf = []
-                else:
-                    buf.append(line)
-
-        try:
-            yield sse_stream_iterator()
-        finally:
-            response.close()
-
-class ServerSentEventMessage(object):
-    """A message from a server sent event stream."""
-
-    def __init__(self, id_, event, data):
-        self.id_ = id_
-        self.event = event
-        self.data = data
-
-    def __str__(self):
-        data_string = repr(self.data)
-        if len(data_string) > 30:
-            data_string = "{}...{}".format(data_string[:12], data_string[-12:])
-        return "{}(id={}, event={}, data={})".format(
-            self.__class__.__name__, self.id_, self.event, data_string
-        )
-
-    @classmethod
-    def from_lines(cls, lines):
-        print(lines)
-        id_ = None
-        event = None
-        data_lines = []
-        for line in lines:
-            if line.startswith("id:"):
-                id_ = int(line[3:].strip())
-            elif line.startswith("event:"):
-                event = line[6:].strip()
-            elif line.startswith("data:"):
-                data_lines.append(line[5:].strip())
-            else:
-                raise ValueError("unexpected sse line: {}".format(line))
-        print(data_lines)
-        data = json.loads("\n".join(data_lines))
-        return cls(id_, event, data)
