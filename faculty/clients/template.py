fce0e6f9db15cbc97e427c0ced6c58e057439cf1
Tomáš Milata <tomas@faculty.ai>
2020-09-18 14:05:46 +0200

    Deserialise publishing responses using marshallow

--- faculty/clients/template.py
+++ faculty/clients/template.py
@@ -15,8 +15,11 @@
 """
 Interact with the Faculty knowledge centre templates.
 """
+from collections import namedtuple
 
-from faculty.clients.base import BadRequest, Conflict, BaseClient
+from marshmallow import fields, post_load
+
+from faculty.clients.base import BaseClient, BaseSchema
 
 
 class TemplateClient(BaseClient):
@@ -57,7 +60,7 @@ class TemplateClient(BaseClient):
             "sourceProjectId": str(source_project_id),
             "sourceDirectory": source_directory,
             "targetDirectory": target_directory,
-            "parameterValues": dict(parameters),
+            "parameterValues": parameters,
         }
         response = self._post_raw(endpoint, json=payload, check_status=False)
         print(response.status_code)
@@ -68,16 +71,23 @@ class TemplateClient(BaseClient):
             response_body = response.json()
             error_code = response_body.get("errorCode")
             if error_code == "resources_validation_failure":
-                _resource_validation_error(response_body)
+                raise ResourceValidationFailureResponseSchema().load(
+                    response_body
+                )
+                # _resource_validation_error(response_body)
             elif error_code == "parameter_validation_failure":
-                msg = "Parameter validation failed:"
-                for e in response_body["errors"]["parameters"]:
-                    msg += "\n" + e
-                raise ParameterValidationFailure(msg)
+                raise ParameterValidationFailureSchema().load(response_body)
+                # msg = "Parameter validation failed:"
+                # for e in response_body["errors"]["parameters"]:
+                #     msg += "\n" + e
+                # raise ParameterValidationFailure(msg)
             elif error_code == "template_retrieval_failure":
-                raise _retrieval_error(response_body)
+                raise TemplateRetrievalFailureResponseSchema().load(
+                    response_body
+                )
+                # raise _retrieval_error(response_body)
             elif error_code == "default_parameters_parsing_error":
-                raise DefaultParametersParsingError(response_body["error"])
+                raise DefaultParametersParsingErrorSchema().load(response_body)
             elif error_code == "generic_parsing_failure":
                 raise TemplateRetrievalFailure(response_body["error"])
             # TODO more cases
@@ -87,58 +97,58 @@ class TemplateClient(BaseClient):
             _unexpected_response(response)
 
 
-def _map_prefix(prefix, errors):
-    return [prefix + e for e in errors]
-
-
-def _one_per_line(error_message_lists):
-    messages = [msg for sublist in error_message_lists for msg in sublist]
-    return "\n".join(messages)
-
-
-def _retrieval_error(response_body):
-    errors = response_body["errors"]
-    apps = errors["apps"]
-    apis = errors["apis"]
-    envs = errors["environments"]
-    jobs = errors["jobs"]
-    message_lists = [
-        _map_prefix(prefix, errors)
-        for prefix, errors in [
-            ("Error reading app resource definition: ", apps),
-            ("Error reading API resource definition: ", apis),
-            ("Error reading environment resource definition: ", envs),
-            ("Error reading app job definition: ", jobs),
-        ]
-    ]
-    raise TemplateRetrievalFailure(_one_per_line(message_lists))
-
-
-def _resource_validation_error(response_body):
-    errors = response_body["errors"]
-    apps = errors["apps"]
-    apis = errors["apis"]
-    envs = errors["environments"]
-    jobs = errors["jobs"]
-    workspace = errors["workspace"]
-    message_lists = [
-        _map_prefix(prefix, errors)
-        for prefix, errors in [
-            ("App subdomain already exists: ", apps["subdomainConflicts"]),
-            ("App name already exists: ", apps["nameConflicts"]),
-            ("Invalid app working directory: ", apps["invalidWorkingDirs"]),
-            ("API subdomain already exists: ", apis["subdomainConflicts"]),
-            ("API name already exists: ", apis["nameConflicts"]),
-            ("Invalid API working directory: ", apis["invalidWorkingDirs"]),
-            ("Environment name already exists: ", envs["nameConflicts"]),
-            ("Invalid environment name: ", envs["invalidNames"]),
-            ("Job name already exists: ", jobs["nameConflicts"]),
-            ("Invalid job name: ", jobs["invalidNames"]),
-            ("Invalid job working directory: ", jobs["invalidWorkingDirs"]),
-            ("Workspace file conflicts: ", workspace["nameConflicts"]),
-        ]
-    ]
-    raise ResourceValidationFailure(_one_per_line(message_lists))
+# def _map_prefix(prefix, errors):
+#     return [prefix + e for e in errors]
+#
+#
+# def _one_per_line(error_message_lists):
+#     messages = [msg for sublist in error_message_lists for msg in sublist]
+#     return "\n".join(messages)
+#
+#
+# def _retrieval_error(response_body):
+#     errors = response_body["errors"]
+#     apps = errors["apps"]
+#     apis = errors["apis"]
+#     envs = errors["environments"]
+#     jobs = errors["jobs"]
+#     message_lists = [
+#         _map_prefix(prefix, errors)
+#         for prefix, errors in [
+#             ("Error reading app resource definition: ", apps),
+#             ("Error reading API resource definition: ", apis),
+#             ("Error reading environment resource definition: ", envs),
+#             ("Error reading app job definition: ", jobs),
+#         ]
+#     ]
+#     raise TemplateRetrievalFailure(_one_per_line(message_lists))
+#
+#
+# def _resource_validation_error(response_body):
+#     errors = response_body["errors"]
+#     apps = errors["apps"]
+#     apis = errors["apis"]
+#     envs = errors["environments"]
+#     jobs = errors["jobs"]
+#     workspace = errors["workspace"]
+#     message_lists = [
+#         _map_prefix(prefix, errors)
+#         for prefix, errors in [
+#             ("App subdomain already exists: ", apps["subdomainConflicts"]),
+#             ("App name already exists: ", apps["nameConflicts"]),
+#             ("Invalid app working directory: ", apps["invalidWorkingDirs"]),
+#             ("API subdomain already exists: ", apis["subdomainConflicts"]),
+#             ("API name already exists: ", apis["nameConflicts"]),
+#             ("Invalid API working directory: ", apis["invalidWorkingDirs"]),
+#             ("Environment name already exists: ", envs["nameConflicts"]),
+#             ("Invalid environment name: ", envs["invalidNames"]),
+#             ("Job name already exists: ", jobs["nameConflicts"]),
+#             ("Invalid job name: ", jobs["invalidNames"]),
+#             ("Invalid job working directory: ", jobs["invalidWorkingDirs"]),
+#             ("Workspace file conflicts: ", workspace["nameConflicts"]),
+#         ]
+#     ]
+#     raise ResourceValidationFailure(_one_per_line(message_lists))
 
 
 def _unexpected_response(response):
@@ -150,16 +160,196 @@ class TemplateException(Exception):
 
 
 class DefaultParametersParsingError(TemplateException):
-    pass
+    def __init__(self, error):
+        self.errors = error
 
 
-class TemplateRetrievalFailure(TemplateException):
-    pass
+class DefaultParametersParsingErrorSchema(BaseSchema):
+    error = fields.String(required=True)
+
+    @post_load
+    def make_error(self, data, **kwargs):
+        return DefaultParametersParsingError(**data)
+
+
+class AppValidationFailureSchema(BaseSchema):
+    subdomain_conflicts = fields.List(
+        fields.String(), data_key="subdomainConflicts", required=True
+    )
+    name_conflicts = fields.List(
+        fields.String(), data_key="nameConflicts", required=True
+    )
+    invalid_working_dirs = fields.List(
+        fields.String(), data_key="invalidWorkingDirs", required=True
+    )
+
+    @post_load
+    def make_failure(self, data, **kwargs):
+        return AppValidationFailure(**data)
+
+
+AppValidationFailure = namedtuple(
+    "AppValidationFailure",
+    ["subdomain_conflicts", "name_conflicts", "invalid_working_dirs"],
+)
+
+
+class ApiValidationFailureSchema(BaseSchema):
+    subdomain_conflicts = fields.List(
+        fields.String(), data_key="subdomainConflicts", required=True
+    )
+    name_conflicts = fields.List(
+        fields.String(), data_key="nameConflicts", required=True
+    )
+    invalid_working_dirs = fields.List(
+        fields.String(), data_key="invalidWorkingDirs", required=True
+    )
+
+    @post_load
+    def make_failure(self, data, **kwargs):
+        return ApiValidationFailure(**data)
+
+
+ApiValidationFailure = namedtuple(
+    "ApiValidationFailure",
+    ["subdomain_conflicts", "name_conflicts", "invalid_working_dirs"],
+)
+
+
+class EnvironmentValidationFailureSchema(BaseSchema):
+    name_conflicts = fields.List(
+        fields.String(), data_key="nameConflicts", required=True
+    )
+    invalid_names = fields.List(
+        fields.String(), data_key="invalidNames", required=True
+    )
+
+    @post_load
+    def make_failure(self, data, **kwargs):
+        return EnvironmentValidationFailure(**data)
+
+
+EnvironmentValidationFailure = namedtuple(
+    "EnvironmentValidationFailure", ["name_conflicts", "invalid_names"]
+)
+
+
+class JobValidationFailureSchema(BaseSchema):
+    name_conflicts = fields.List(
+        fields.String(), data_key="nameConflicts", required=True
+    )
+    invalid_working_dirs = fields.List(
+        fields.String(), data_key="invalidWorkingDirs", required=True
+    )
+    invalid_names = fields.List(
+        fields.String(), data_key="invalidNames", required=True
+    )
+
+    @post_load
+    def make_failure(self, data, **kwargs):
+        return JobValidationFailure(**data)
+
+
+JobValidationFailure = namedtuple(
+    "JobValidationFailure",
+    ["name_conflicts", "invalid_working_dirs", "invalid_names"],
+)
+
+
+class WorkspaceValidationFailureSchema(BaseSchema):
+    name_conflicts = fields.List(
+        fields.String(), data_key="nameConflicts", required=True
+    )
+
+    @post_load
+    def make_failure(self, data, **kwargs):
+        return WorkspaceValidationFailure(**data)
+
+
+WorkspaceValidationFailure = namedtuple(
+    "WorkspaceValidationFailure", ["name_conflicts"]
+)
+
+
+class ResourceValidationFailuresSchema(BaseSchema):
+    apps = fields.Nested(AppValidationFailureSchema(), required=True)
+    apis = fields.Nested(ApiValidationFailureSchema(), required=True)
+    environments = fields.Nested(
+        EnvironmentValidationFailureSchema(), required=True
+    )
+    jobs = fields.Nested(JobValidationFailureSchema(), required=True)
+    workspace = fields.Nested(
+        WorkspaceValidationFailureSchema(), required=True
+    )
+
+
+class ResourceValidationFailureResponseSchema(BaseSchema):
+    errors = fields.Nested(ResourceValidationFailuresSchema(), required=True)
+
+    @post_load
+    def make_failure(self, data, **kwargs):
+        return ResourceValidationFailure.from_errors(**data)
 
 
 class ResourceValidationFailure(TemplateException):
-    pass
+    def __init__(self, apps, apis, environments, jobs, workspace):
+        self.apps = apps
+        self.apis = apis
+        self.environments = environments
+        self.jobs = jobs
+        self.workspace = workspace
+
+    @staticmethod
+    def from_errors(errors):
+        return ResourceValidationFailure(
+            apps=errors["apps"],
+            apis=errors["apis"],
+            environments=errors["environments"],
+            jobs=errors["jobs"],
+            workspace=errors["workspace"],
+        )
 
 
 class ParameterValidationFailure(TemplateException):
-    pass
+    def __init__(self, errors):
+        self.errors = errors
+
+
+class ParameterValidationFailureSchema(BaseSchema):
+    errors = fields.List(fields.String(), required=True)
+
+    @post_load()
+    def make_failure(self, data, **kwargs):
+        return ParameterValidationFailure(**data)
+
+
+class TemplateRetrievalFailure(TemplateException):
+    def __init__(self, apps, apis, environments, jobs):
+        self.apps = apps
+        self.apis = apis
+        self.environments = environments
+        self.jobs = jobs
+
+    @staticmethod
+    def from_errors(errors):
+        return TemplateRetrievalFailure(
+            apps=errors["apps"],
+            apis=errors["apis"],
+            environments=errors["environments"],
+            jobs=errors["jobs"],
+        )
+
+
+class TemplateRetrievalFailureSchema(BaseSchema):
+    apps = fields.List(fields.String(), required=True)
+    apis = fields.List(fields.String(), required=True)
+    environments = fields.List(fields.String(), required=True)
+    jobs = fields.List(fields.String(), required=True)
+
+
+class TemplateRetrievalFailureResponseSchema(BaseSchema):
+    errors = fields.Nested(TemplateRetrievalFailureSchema(), required=True)
+
+    @post_load()
+    def make_failure(self, data, **kwargs):
+        return TemplateRetrievalFailure.from_errors(**data)
--- tests/clients/test_template.py
+++ tests/clients/test_template.py
@@ -15,23 +15,201 @@
 
 import uuid
 
-from faculty.clients.template import TemplateClient
+import pytest
 
+from faculty.clients.template import (
+    TemplateClient,
+    ResourceValidationFailure,
+    AppValidationFailure,
+    ApiValidationFailure,
+    EnvironmentValidationFailure,
+    JobValidationFailure,
+    WorkspaceValidationFailure,
+    ParameterValidationFailure,
+    TemplateRetrievalFailure,
+    DefaultParametersParsingError,
+)
 
-PROJECT_ID = uuid.uuid4()
+SOURCE_PROJECT_ID = uuid.uuid4()
+TARGET_PROJECT_ID = uuid.uuid4()
 
 
 def test_publish_new(mocker):
     mocker.patch.object(TemplateClient, "_post_raw")
 
     client = TemplateClient(mocker.Mock())
-    client.publish_new(PROJECT_ID, "template name", "source/dir")
+    client.publish_new(SOURCE_PROJECT_ID, "template name", "source/dir")
 
     TemplateClient._post_raw.assert_called_once_with(
         "template",
         json={
-            "sourceProjectId": str(PROJECT_ID),
+            "sourceProjectId": str(SOURCE_PROJECT_ID),
             "sourceDirectory": "source/dir",
             "name": "template name",
         },
     )
+
+
+def test_add_to_project_from_directory(mocker):
+    mock_response = mocker.Mock()
+    mock_response.status_code = 202
+    mocker.patch.object(
+        TemplateClient, "_post_raw", return_value=mock_response
+    )
+
+    client = TemplateClient(mocker.Mock())
+    client.add_to_project_from_directory(
+        SOURCE_PROJECT_ID,
+        "source/dir",
+        TARGET_PROJECT_ID,
+        "target/dir",
+        {"paramA": "a", "paramB": "B"},
+    )
+
+    TemplateClient._post_raw.assert_called_once_with(
+        "project/{}/apply".format(TARGET_PROJECT_ID),
+        json={
+            "sourceProjectId": str(SOURCE_PROJECT_ID),
+            "sourceDirectory": "source/dir",
+            "targetDirectory": "target/dir",
+            "parameterValues": {"paramA": "a", "paramB": "B"},
+        },
+        check_status=False,
+    )
+
+
+@pytest.mark.parametrize(
+    "mock_response_payload, expected_exception",
+    [
+        (
+            {
+                "errorCode": "resources_validation_failure",
+                "errors": {
+                    "apps": {
+                        "subdomainConflicts": ["app-subdomain-1"],
+                        "nameConflicts": [
+                            "test-app-name-1",
+                            "test-app-name-2",
+                        ],
+                        "invalidWorkingDirs": ["invalid/app/dir"],
+                    },
+                    "apis": {
+                        "subdomainConflicts": ["api-subdomain-1"],
+                        "nameConflicts": ["test-api-name"],
+                        "invalidWorkingDirs": ["invalid/API/dir"],
+                    },
+                    "environments": {
+                        "nameConflicts": ["test-env-name"],
+                        "invalidNames": ["invalid#env"],
+                    },
+                    "jobs": {
+                        "nameConflicts": ["test-job-name"],
+                        "invalidWorkingDirs": [],
+                        "invalidNames": ["invalid#job"],
+                    },
+                    "workspace": {"nameConflicts": ["test-file-path"]},
+                },
+            },
+            ResourceValidationFailure(
+                apps=AppValidationFailure(
+                    subdomain_conflicts=["app-subdomain-1"],
+                    name_conflicts=["test-app-name-1", "test-app-name-2"],
+                    invalid_working_dirs=["invalid/app/dir"],
+                ),
+                apis=ApiValidationFailure(
+                    subdomain_conflicts=["api-subdomain-1"],
+                    name_conflicts=["test-api-name"],
+                    invalid_working_dirs=["invalid/API/dir"],
+                ),
+                environments=EnvironmentValidationFailure(
+                    name_conflicts=["test-env-name"],
+                    invalid_names=["invalid#env"],
+                ),
+                jobs=JobValidationFailure(
+                    name_conflicts=["test-job-name"],
+                    invalid_working_dirs=[],
+                    invalid_names=["invalid#job"],
+                ),
+                workspace=WorkspaceValidationFailure(
+                    name_conflicts=["test-file-path"]
+                ),
+            ),
+        ),
+        (
+            {
+                "errorCode": "parameter_validation_failure",
+                "errors": ["test param 1 error", "test param 2 error"],
+            },
+            ParameterValidationFailure(
+                errors=["test param 1 error", "test param 2 error"]
+            ),
+        ),
+        (
+            {
+                "errorCode": "template_retrieval_failure",
+                "errors": {
+                    "apps": [],
+                    "apis": [],
+                    "environments": [],
+                    "jobs": [],
+                },
+            },
+            TemplateRetrievalFailure(
+                apps=[], apis=[], environments=[], jobs=[]
+            ),
+        ),
+        (
+            {
+                "errorCode": "default_parameters_parsing_error",
+                "error": "parameter error",
+            },
+            DefaultParametersParsingError(error=["parameter error"]),
+        ),
+    ],
+)
+def test_add_to_project_from_directory_errors(
+    mocker, mock_response_payload, expected_exception
+):
+    mock_response = mocker.Mock()
+    mock_response.status_code = 400
+    mocker.patch.object(
+        mock_response, "json", return_value=mock_response_payload
+    )
+    mocker.patch.object(
+        TemplateClient, "_post_raw", return_value=mock_response
+    )
+
+    client = TemplateClient(mocker.Mock())
+    with pytest.raises(type(expected_exception)) as e:
+        client.add_to_project_from_directory(
+            SOURCE_PROJECT_ID,
+            "source/dir",
+            TARGET_PROJECT_ID,
+            "target/dir",
+            {"paramA": "a", "paramB": "B"},
+        )
+
+    assert e.value.args == expected_exception.args
+
+    TemplateClient._post_raw.assert_called_once_with(
+        "project/{}/apply".format(TARGET_PROJECT_ID),
+        json={
+            "sourceProjectId": str(SOURCE_PROJECT_ID),
+            "sourceDirectory": "source/dir",
+            "targetDirectory": "target/dir",
+            "parameterValues": {"paramA": "a", "paramB": "B"},
+        },
+        check_status=False,
+    )
+
+
+#             """App subdomain already exists: app-subdomain-1
+# App name already exists: test-app-name-1
+# App name already exists: test-app-name-2
+# Invalid app working directory: invalid/app/dir
+# API subdomain already exists: api-subdomain-1
+# API name already exists: test-api-name
+# Invalid API working directory: invalid/API/dir
+# Environment name already exists: test-env-name
+# Invalid environment name: invalid#env
+# """,
